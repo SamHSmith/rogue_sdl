41fce441ad8ac2b014b66cd647f32928f2f6f1c8
Sam H Smith <sam.henning.smith@protonmail.com>
2021-05-16 16:55:09 +0200

    pull in functions

--- src/main.c
+++ src/main.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <stdbool.h>
-#include "game.h"
+//#include "game.h"
 #include "player.h"
 
 int main(int argc, char* argv[])
 {
-    SDL_Window* window;
-    bool running = false;
-    SDL_Renderer* renderer;
-    init(window, renderer, "Woods", 640, 640, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, false, &running);
+    if(SDL_Init(SDL_INIT_EVERYTHING)!=0)
+    { return 1; }
+    printf("SDL initialized.\n");
+
+    bool fullscreen = false;
+    if(fullscreen)
+    {
+        int flags = 0;
+        flags = SDL_WINDOW_FULLSCREEN;
+    }
+    SDL_Window* window = SDL_CreateWindow(
+                            "Woods",
+                            SDL_WINDOWPOS_CENTERED,
+                            SDL_WINDOWPOS_CENTERED,
+                            640, 640,
+                            SDL_WINDOW_RESIZABLE
+    );
+    if(!window)
+    { printf("Failed to create window\n"); return 2; }
+    printf("Window Created.\n");
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    if(!renderer)
+    { printf("Failed to create renderer\n"); return 3; }
+    printf("Renderer Created.\n");
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+
     int cnt = 0;
     Player player;
     player.xpos = 0;
@@ -19,21 +43,48 @@ int main(int argc, char* argv[])
     const int frame_delay = 1000/FPS;
     Uint32 time_start;
     int frame_time;
+    bool running = true;
     while(running)
     {
+        { // Input
+            SDL_Event event;
+            SDL_PollEvent(&event);
+            switch(event.type)
+            {
+                case SDL_QUIT:
+                    running = false;
+                    break;
+            }
+        }
+
         cnt++;
-		player.xpos++;
+        player.xpos++;
         time_start = SDL_GetTicks();
-        handle_events(window, renderer, &running);
-        update(cnt, &player);
-		render(renderer, &player);
+
+        { // Update
+            printf("%d\n", cnt);
+            player_update(&player);
+        }
+
+        { // Render
+            SDL_RenderClear(renderer);
+            player_draw(renderer, &player);
+            SDL_RenderPresent(renderer);
+        }
+
+
         frame_time = SDL_GetTicks() - time_start;
         if(frame_delay > frame_time)
         {
             SDL_Delay(frame_delay - frame_time);
         }
     }
-    clean_sdl(window, renderer, &player);
-    
+
+    // Cleanup
+    SDL_DestroyTexture(player.player_tex);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    printf("SDL Quit.\n");
 	return 0;
 }
